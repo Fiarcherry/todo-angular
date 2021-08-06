a9d76bb519ad3ef1f274e7d009a9993ea12f2801
fiarcherry <fiarchersschannel@gmail.com>
2021-08-06 17:34:46 +0300

    Исправления в маршрутизации

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { Filter } from './models/filter.model';
 
 const routes: Routes = [
   { path: '', redirectTo: 'todos/all', pathMatch: 'full' },
-  { path: 'todos/all', component: TodoListComponent },
-  { path: 'todos/active', component: TodoListComponent },
-  { path: 'todos/done', component: TodoListComponent }
+  { path: 'todos/all', component: TodoListComponent, data: {name: Filter.All} },
+  { path: 'todos/active', component: TodoListComponent, data: {name: Filter.Active} },
+  { path: 'todos/done', component: TodoListComponent, data: {name: Filter.Done} },
+  { path: '**', redirectTo: 'todos/all'}
 ];
 
 @NgModule({
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { ActivatedRoute, Data } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { Filter } from 'src/app/models/filter.model';
 import { Todo } from 'src/app/models/todo.model';
+import { FilterService } from 'src/app/services/filter.service';
 import { TodoService } from 'src/app/services/todos.service'
 
 @Component({
@@ -10,13 +13,39 @@ import { TodoService } from 'src/app/services/todos.service'
 })
 export class TodoListComponent implements OnInit {
   todos$: Observable<Todo[]>
+  private routeSubscription: Subscription;
 
   constructor(
+    private route: ActivatedRoute,
     private todoService: TodoService,
+    private filterService: FilterService,
   ) {
-     this.todos$ = this.todoService.getAll()
+    this.routeSubscription = this.route.data.subscribe(data => this.setFilterByRoute(data))
+
+    this.todos$ = this.todoService.getAll()
   }
 
   ngOnInit(): void {
+  }
+  
+  setFilterByRoute(data: Data): void {
+    switch (data.name) {
+      case Filter.Active: {
+        this.filterService.set(Filter.Active)
+        break;
+      }
+      case Filter.Done: {
+        this.filterService.set(Filter.Done)
+        break;
+      }
+      default: {
+        this.filterService.set(Filter.All)
+        break;
+      }
+    }
+  }
+  
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe()
   }
 }
